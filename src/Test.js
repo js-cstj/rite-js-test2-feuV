958183489e79d9de687c6cf21e79cea4209d84d0
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-08-05 18:39:16 -0400

    first commit

---
+++ src/Test.js
@@ -0,0 +1,21 @@
+export default class Test {
+    static feuV() {
+        // <div class="body" id="app">
+        //     <div class="feu vertical">
+        //         <table>
+        //             <tr>
+        //                 <td style="background-color: red;"></td>
+        //             </tr>
+        //             <tr>
+        //                 <td style="background-color: yellow;"></td>
+        //             </tr>
+        //             <tr>
+        //                 <td style="background-color: green;"></td>
+        //             </tr>
+        //         </table>
+        //     </div>
+        // </div>
+        var app, div, table, tr, td;
+           
+    }
+}
